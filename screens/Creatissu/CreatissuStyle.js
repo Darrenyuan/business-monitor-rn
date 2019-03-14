1a5cba4aa8c0af77bb5f61d33e7f13d62d0b8e9b
wangyue <1848886706@qq.com>
2019-03-14 09:28:18 +0800

    create issue

--- screens/Creatissu/CreatissuStyle.js
+++ screens/Creatissu/CreatissuStyle.js
@@ -5,5 +5,76 @@ import { Colors } from "../../constants";
 // const SCREEN_HEIGHT = Dimensions.get("window").height;
 
 export default {
-  
+    container: {
+        flex: 1
+    },
+    title:{
+        marginTop:40,
+        marginLeft:20,
+        fontSize:22,
+        textAlign: 'left',
+        height:40
+    },
+    inputContainer:{
+        borderWidth:1,
+        borderStyle:'dashed',
+        borderColor:'#ccc',
+        marginTop: 20,
+        width:320,
+        marginLeft:20,
+        height:105,
+        borderRadius:6
+    },    
+    textInput:{
+        textAlignVertical:'top',
+        justifyContent: "flex-start",
+        alignItems:'flex-start',
+        height:105,
+        paddingVertical: 0,
+        paddingLeft: 5, 
+        fontSize: 16, 
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        color: '#000',
+        padding: 10,
+        margin: 40
+    },
+    modalDropdownContainer:{
+        marginTop:30,
+        width:320,
+        marginLeft:20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    modalDropdown:{
+        backgroundColor: "#40AFFF",
+        width: 130,
+        height: 50,
+        borderRadius: 4,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    textStyle:{
+        color:'white',
+        fontSize: 18,
+    },
+    dropdownStyle:{
+        // flex:1,
+        // width: 130,
+        // borderRadius: 4,
+        // justifyContent: "center",
+        // alignItems: "center",
+        // backgroundColor: "red",
+        // marginRight:30
+    }
 };
--- screens/Creatissu/index.js
+++ screens/Creatissu/index.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import {
+    TextInput,
     StyleSheet,
     Text,
     View,
-    ImageBackground,
-    Dimensions
+    Image
 } from "react-native";
 import { CheckBox, Button, } from "react-native-elements";
+// import CameraKitCamera from "react-native-camera-kit";
+import CameraKitCamera from "./Camera";
 import { Font } from "expo";
 import Icon from "react-native-vector-icons/FontAwesome";
-import CreatissuStyle from "./CreatissuStyle";
+import CameraExample from "./Camera";
 import { t } from "../../services/i18n";
 import { bindActionCreators } from "redux";
 import * as actions from "../../services/redux/actions";
@@ -17,32 +19,82 @@ import { connect } from "react-redux";
 import { NavigationActions } from "react-navigation";
 import ModalDropdown from 'react-native-modal-dropdown';
 import Projects from "../Projects";
-
+import CreatissuStyle from './CreatissuStyle';
 const styles = StyleSheet.create({ ...CreatissuStyle });
 class Creatissu extends Component{
+  constructor(props) {
+    super(props);
+    this.camaraRef= React.createRef();
+    this.state = {
+      
+    };
+  }
   render(){
+    console.log('tytytytytytytytytytytytytytytytytytytytytyty');
     return(
-      <View>
-
-        {/* <ModalDropdown 
-           defaultValue=
-           options={}
-           style={{
-            height: 50,
-            width: 250,
-            backgroundColor: "blue"
-           }}
+      <View style={styles.container}>
+       <Text style={styles.title}>{t("drawer.creatissu_label")}</Text>
+        <View style = {styles.inputContainer}>
+          <TextInput
+              autoFocus = {true}
+              placeholder = {t("screen.creatissu_textinput")} 
+              placeholderTextColor = {'#BBBBBB'}
+              underlineColorAndroid = {'transparent'} 
+              style={styles.textInput}
+              multiline
+          />
+        </View>
+        <View>
+          <Text>11111111</Text>
+          <CameraExample/>
+       {/* <CameraKitCamera
+        ref={this.camaraRef}
+            style={{
+            flex: 1,
+            backgroundColor: 'white'
+          }}
+          cameraOptions={{
+            flashMode: 'auto',             // on/off/auto(default)
+            focusMode: 'on',               // off/on(default)
+            zoomMode: 'on',                // off/on(default)
+            ratioOverlay:'1:1',            // optional, ratio overlay on the camera and crop the image seamlessly
+            ratioOverlayColor: '#00000077' // optional
+          }}
         /> */}
+        <Text>333333333</Text>
+        </View>
+        <View style={styles.modalDropdownContainer}>
+            <ModalDropdown 
+              defaultValue={t("screen.creatissu_modalDropdown1")}
+              options={[t("screen.creatissu_modalDropdown1_item1"),t("screen.creatissu_modalDropdown1_item2"),
+                        t("screen.creatissu_modalDropdown1_item3"),t("screen.creatissu_modalDropdown1_item4")]}
+              style={styles.modalDropdown}
+              textStyle={styles.textStyle}
+              // dropdownStyle={styles.dropdownStyle}
+              adjustFrame={styles.dropdownPosition} 
+              // dropdownTextStyle={styles.dropdownText} 
+            /> 
+            <ModalDropdown 
+              defaultValue={t("screen.creatissu_modalDropdown2")}
+              options={[t("screen.creatissu_modalDropdown2_item1"),t("screen.creatissu_modalDropdown2_item2"),
+                        t("screen.creatissu_modalDropdown2_item3"),t("screen.creatissu_modalDropdown2_item4"),
+                        t("screen.creatissu_modalDropdown2_item5")]}
+              style={styles.modalDropdown}
+              textStyle={styles.textStyle} 
+              dropdownStyle={styles.dropdownStyle}
+            /> 
+          </View>
         <Button 
-          title="Solid Button"
-          // buttonStyle={{
-          //   height: 50,
-          //   width: 250,
-          //   backgroundColor: "transparent",
-          //   borderWidth: 2,
-          //   borderColor: "white",
-          //   borderRadius: 30
-          // }}
+          title={t("screen.creatissu_submit")}
+          buttonStyle={{
+            height: 50,
+            width: 320,
+            borderWidth: 2,
+            borderColor: "white",
+            borderRadius: 30,
+            marginLeft:20,
+            marginTop:200
+          }}
         />
       </View>
     );
